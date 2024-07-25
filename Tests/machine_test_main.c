f435dff8640a244fa51a5d0304ea9d45b933a801
HellaJeff <robbins.jeffry@gmail.com>
2024-07-25 03:40:47 -0700

    Update machine_test_main.c
    Removed unused header, updated include to reflect file structure. Added missing free() for the test machine struct pointer.

--- Tests/machine_test_main.c
+++ Tests/machine_test_main.c
@@ -1,5 +1,4 @@
-#include <assert.h>
-#include "rr_machine.h"
+#include "../rr_machine.h"
 
 s32 main(s32 argc, const char **argv) {
 	
@@ -10,6 +9,8 @@ s32 main(s32 argc, const char **argv) {
 	machine_run_full(test_machine, 0);
 	
 	machine_save(test_machine, "./test_a_out.bin");
+	
+	free(test_machine);
 	
 	return 0;
 	
