fda08607af83d59b5a64f77dbd6de5d0a271186c
HellaJeff <robbins.jeffry@gmail.com>
2024-07-29 16:56:29 -0700

    Updated Command List
    Condensed jump/branch commands into a single command that can use one, both, or neither status register flag high or low (independently) to branch. Added load from memory with register offset (LDM) and rotate register (ROT) using the newly freed up opcodes and adjusted opcode placement. Added new operations to the test file to ensure working order.

--- rr_machine.c
+++ rr_machine.c
@@ -6,16 +6,16 @@ const char *instruction_mnemonics[16] = {
 	"AND",
 	"ORR",
 	"XOR",
+	"ROT",
 	"LDI",
 	"LDM",
+	"LDR",
 	"STO",
 	"PSH",
 	"POP",
-	"JMP",
 	"JSR",
 	"RET",
-	"BRZ",
-	"BRC",
+	"BRA",
 	"MDF"
 };
 
@@ -75,9 +75,9 @@ void machine_decode(rr_machine_t *machine) {
 		// HLT - 0___
 		// HaLT machine execution
 		case 0x0:
-		// RET - C___
+		// RET - D___
 		// RETurn from subroutine, Mem[++SP] -> PC
-		case 0xC:
+		case 0xD:
 			break;
 		// ADC - 1RST
 		// ADd with Carry, adds S + T + C -> R
@@ -96,47 +96,64 @@ void machine_decode(rr_machine_t *machine) {
 		// XOR registers, S ^ T -> R
 		// Sets zero if R = 0, clears otherwise
 		case 0x4:
+		// ROT - 5RST
+		// ROTate register, S (<< | >>) T -> R
+		// T[4] controls direction, 0 for left, 1 for right
+		// Sets zero if R = 0, clears otherwise
+		// Carry will be set according to the last bit from S to be rotated out
+		case 0x5:
 			machine->operands[1] = (machine->instruction_register >> 8) & 0xF;
 			machine->operands[2] = (machine->instruction_register >> 4) & 0xF;
 			machine->operands[3] = machine->instruction_register & 0xF;
 			break;
-		// LDI - 5RXX
+		// LDI - 6RXX
 		// LoaD Immediate, XX -> R
 		// Sets zero if R = 0, clears otherwise
-		case 0x5:
-		// LDM - 6RMM
+		case 0x6:
+		// LDM - 7RMM
 		// LoaD from Memory, Mem[MM] -> R
 		// Sets zero if R = 0, clears otherwise
-		case 0x6:
-		// STO - 7RMM
+		case 0x7:
+		// STO - 9RMM
 		// STOre register, R -> Mem[MM]
 		// Sets zero if R = 0, clears otherwise
-		case 0x7:
+		case 0x9:
 			machine->operands[1] = (machine->instruction_register >> 8) & 0xF;
 			machine->operands[2] = machine->instruction_register & 0xFF;
 			break;
-		// PSH - 8R__
-		// PuSH register to stack, R -> [SP--]
+		// LDR - 8_RS
+		// LoaD with Register offset, Mem[S] -> R
 		// Sets zero if R = 0, clears otherwise
 		case 0x8:
-		// POP - 9R__
+			machine->operands[1] = (machine->instruction_register >> 4) & 0xF;
+			machine->operands[2] = machine->instruction_register & 0xF;
+			break;
+		// PSH - AR__
+		// PuSH register to stack, R -> [SP--]
+		case 0xA:
+		// POP - BR__
 		// POP stack to register, [++SP] -> R
-		case 0x9:
+		// Sets zero if R = 0
+		case 0xB:
 			machine->operands[1] = (machine->instruction_register >> 8) & 0xF;
 			break;
-		// JMP - A_XX
-		// unconditional JuMP, XX -> PC
-		case 0xA:
-		// JSR - B_XX
+		// JSR - C_XX
 		// Jump to SubRoutine, PC + 2 -> Mem[SP--], XX -> PC
-		case 0xB:
-		// BRZ - D_XX
-		// BRanch on Zero set, Z ? (XX : PC + 2) -> PC
-		case 0xD:
-		// BRC - E_XX
-		// BRanch on Carry set, C ? (XX : PC + 2) -> PC
+		case 0xC:
+			machine->operands[1] = machine->instruction_register & 0xFF;
+			break;
+		// BRA - EIXX
+		// BRAnch on flag conditions, I ? (XX : PC + 2) -> PC
+		// I consists of two 2-bit fields
+		// 01 -> which flags should be considered
+		// 23 -> the state of considered flags to branch on
+		// ex 1, 1100 -> branch only if both zero and carry are cleared
+		// ex 2, 0111 -> branch only if carry is set, ignore the zero flag (interchangeable with 0101)
+		// If I <= 3, this will be an unconditional jump
 		case 0xE:
-			machine->operands[1] = (machine->instruction_register) & 0xFF;
+			machine->operands[1] = (machine->instruction_register >> 10) & 0x3;
+			machine->operands[2] = (machine->instruction_register >> 8) & 0x3;
+			machine->operands[3] = machine->instruction_register & 0xFF;
 			break;
 		// MDF - FI__
 		// MoDify Flag, sets the given status flag to the specified value
@@ -206,8 +223,50 @@ void machine_execute(rr_machine_t *machine) {
 		
 			break;
 			
-		// Load immediate
+		// Rotate register
 		case 0x5:
+		
+			// Early break if this should not rotate at all
+			if((REG(machine, machine->operands[3]) & 0b0111) == 0)
+				break;
+		
+			{
+		
+				u16 temp;
+				u8 shift_count = REG(machine, machine->operands[3]) & 0b0111;
+		
+				// Rotate right
+				if(REG(machine, machine->operands[3]) & 0b1000) {
+					
+					temp = (machine->status_register & 0b01) << (8 - shift_count);
+					temp |= REG(machine, machine->operands[2]) >> shift_count;
+					temp |= REG(machine, machine->operands[2]) << (9 - shift_count);
+					
+					
+					machine->status_register = (machine->status_register & 0b1100) | (!REG(machine, machine->operands[1]) << 1) | ((REG(machine, machine->operands[2]) >> (shift_count - 1)) & 1);
+					
+					REG(machine, machine->operands[1]) = temp & 0xFF;
+					
+				}
+				// Rotate left
+				else {
+					
+					temp = (machine->status_register & 0b01) << (shift_count - 1);
+					temp |= REG(machine, machine->operands[2]) >> (9 - shift_count);
+					temp |= REG(machine, machine->operands[2]) << shift_count;
+					
+					machine->status_register = (machine->status_register & 0b1100) | (!REG(machine, machine->operands[1]) << 1) | ((REG(machine, machine->operands[2]) >> (8 - shift_count)) & 1);
+					
+					REG(machine, machine->operands[1]) = temp & 0xFF;
+
+				}
+				
+			}
+	
+			break;
+			
+		// Load immediate
+		case 0x6:
 		
 			REG(machine, machine->operands[1]) = machine->operands[2];
 			
@@ -217,7 +276,7 @@ void machine_execute(rr_machine_t *machine) {
 			break;
 			
 		// Load from memory
-		case 0x6:
+		case 0x7:
 		
 			REG(machine, machine->operands[1]) = MEM(machine, machine->operands[2]);
 			
@@ -225,9 +284,19 @@ void machine_execute(rr_machine_t *machine) {
 			machine->status_register = (machine->status_register & 0b1101) | ((REG(machine, machine->operands[1]) == 0) << 1);
 		
 			break;
+		
+		// Load from memory with register offset
+		case 0x8:
+		
+			REG(machine, machine->operands[1]) = MEM(machine, REG(machine, machine->operands[2]));
+		
+			// Update status register - [SS_C] -> maintain, [Z] -> set when the result is 0
+			machine->status_register = (machine->status_register & 0b1101) | ((REG(machine, machine->operands[1]) == 0) << 1);
+		
+			break;
 			
 		// Store
-		case 0x7:
+		case 0x9:
 		
 			MEM(machine, machine->operands[2]) = REG(machine, machine->operands[1]);
 			
@@ -237,17 +306,14 @@ void machine_execute(rr_machine_t *machine) {
 			break;
 			
 		// Push register
-		case 0x8:
+		case 0xA:
 		
 			MACHINE_PUSH(machine, REG(machine, machine->operands[1]));
-			
-			// Update status register - [SS_C] -> maintain, [Z] -> set when the result is 0
-			machine->status_register = (machine->status_register & 0b1101) | ((REG(machine, machine->operands[1]) == 0) << 1);
 			
 			break;
 			
 		// Pop to register
-		case 0x9:
+		case 0xB:
 		
 			REG(machine, machine->operands[1]) = MACHINE_POP(machine);
 			
@@ -255,16 +321,9 @@ void machine_execute(rr_machine_t *machine) {
 			machine->status_register = (machine->status_register & 0b1101) | ((REG(machine, machine->operands[1]) == 0) << 1);
 		
 			break;
-			
-		// Jump unconditional
-		case 0xA:
-		
-			machine->program_counter = machine->operands[1] - 2;
-		
-			break;
 		
 		// Jump subroutine
-		case 0xB:
+		case 0xC:
 		
 			MACHINE_PUSH(machine, machine->program_counter + 2);
 			
@@ -273,26 +332,19 @@ void machine_execute(rr_machine_t *machine) {
 			break;
 		
 		// Return from subroutine
-		case 0xC:
+		case 0xD:
 		
 			machine->program_counter = MACHINE_POP(machine);
 		
 			// Return instead of break so we skip adding to the program counter - this would make it problematic if the user wanted to change or read the value in the stack region
 			return;
 		
-		// Branch on zero set
-		case 0xD:
-		
-			if(machine->status_register & 0b0010)
-				machine->program_counter = machine->operands[1] - 2;
-		
-			break;
-			
-		// Branch on carry set
+		// Branch on flag conditions
 		case 0xE:
 		
-			if(machine->status_register & 0b0001)
-				machine->program_counter = machine->operands[1] - 2;
+			// Unconditional jump if I <= 3, since operands[1] will be 0 as 0 & X == 0 always
+			if((machine->operands[1] & ~(machine->status_register ^ machine->operands[2])) == machine->operands[1])
+				machine->program_counter = machine->operands[3] - 2;
 		
 			break;
 			
